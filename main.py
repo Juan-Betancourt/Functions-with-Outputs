cbd50e405d091053f97d05f4034e65c0e7574ec0
Juan Betancourt <juancbetancourt11@gmail.com>
2022-05-17 00:39:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Functions with Outputs
+
+def format_name(f_name, l_name):
+  formated_f_name = (f_name.title())
+  formated_l_name = (l_name.title())
+
+  return f"{formated_f_name} {formated_l_name}"
+
+formated_string = format_name("Angela", "yu")
+print(formated_string)
+# or
+# print(format_name("AnGeLa", "YU"))
+
+output = len("Angela")
